7d512acf8d12017afe5fd57dcac9fc6721f7e531
Nicholas Owens <digitalseraphim@gmail.com>
2014-09-17 21:55:02 -0400

    remote work

--- src/main/java/digitalseraphim/hc/entity/EntityWheelbarrow.java
+++ src/main/java/digitalseraphim/hc/entity/EntityWheelbarrow.java
@@ -7,6 +7,8 @@ import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.item.EntityMinecart;
+import net.minecraft.entity.item.EntityMinecartEmpty;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -14,14 +16,14 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.common.IMinecartCollisionHandler;
 
-public class EntityWheelbarrow extends EntityBoat {
-
-	
-	
-	
+public class EntityWheelbarrow extends Entity {
+    
 	public EntityWheelbarrow(World w) {
 		super(w);
+		this.width = 1.2f;
+		this.height = 1f;
 	}
 
 	@Override
@@ -36,5 +38,34 @@ public class EntityWheelbarrow extends EntityBoat {
 
 	}
 
+	@Override
+	protected void entityInit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void updateBoundingBox(){
+		
+	}
 	
+    /**
+     * Returns a boundingBox used to collide the entity with other entities and blocks. This enables the entity to be
+     * pushable on contact, like boats or minecarts.
+     */
+    public AxisAlignedBB getCollisionBox(Entity p_70114_1_)
+    {
+    	updateBoundingBox();
+        return p_70114_1_.boundingBox;
+    }
+
+    /**
+     * returns the bounding box for this entity
+     */
+    public AxisAlignedBB getBoundingBox()
+    {
+    	updateBoundingBox();
+        return boundingBox;
+    }
+    
+
 }
